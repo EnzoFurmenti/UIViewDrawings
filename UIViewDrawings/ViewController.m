ed01f346c17a651c25313ebd82a7bd36f48786ef
EnzoFurmenti <ducheenzo@mail.ru>
2016-09-05 14:05:05 +0300

    add improvement

--- UIViewDrawings/ViewController.m
+++ UIViewDrawings/ViewController.m
@@ -25,8 +25,6 @@
     // Dispose of any resources that can be recreated.
 }
 
-    
-}
 -(void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator{
     [self.starView setNeedsDisplay];
 
